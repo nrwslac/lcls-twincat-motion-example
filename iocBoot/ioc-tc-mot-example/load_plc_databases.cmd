3af8097a10b655939d05928e2544f2c097083b96
Zachary Lentz <zlentz@slac.stanford.edu>
2021-05-13 15:05:58 -0700

    BLD: rebuild IOC

--- iocBoot/ioc-tc-mot-example/load_plc_databases.cmd
+++ iocBoot/ioc-tc-mot-example/load_plc_databases.cmd
@@ -1,1 +1,3 @@
-dbLoadRecords("tc_mot_example.db", "PORT=ASYN_PLC,PREFIX=PLC:TST:MOT:,IOCNAME=$(IOCNAME)")
+# Total records: 1553
+callbackSetQueueSize(5106)
+dbLoadRecords("tc_mot_example.db", "PORT=ASYN_PLC,PREFIX=PLC:TST:MOT:,IOCNAME=$(IOC),IOC=$(IOC)")
